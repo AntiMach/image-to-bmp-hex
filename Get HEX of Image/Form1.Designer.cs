f5e24dcdd472ca114ff4d2069a443b5143f1e0d3
TheMachinumps <alunopedrofernandes@gmail.com>
2017-04-09 22:32:18 +0100

    First commit (for real)

---
+++ Get HEX of Image/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.Control;
+            this.output.Location = new System.Drawing.Point(15, 101);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(604, 201);
+            this.output.TabIndex = 3;
+            this.output.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 314);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Get HEX from Image";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ Get HEX of Image/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public string HEXofBitmap(Image image)
+        {
+            MemoryStream stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Bmp);
+            byte[] hexarray = stream.ToArray();
+            string hex = BitConverter.ToString(hexarray);
+            return hex.Replace("-"," ");
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog img = new OpenFileDialog();
+            img.Filter = "PNG Files|*.png|JPEG Files|*.jpeg|GIF Files|*.gif|BMP Files|*.bmp";
+            img.Title = "Open an image file to get the hex data from";
+
+            if (img.ShowDialog() == DialogResult.OK)
+            {
+                output.Text = HEXofBitmap(Image.FromFile(img.FileName));
+            }
+        }
+    }
+}
